6b97a1acd69f3ebe022c3fec6771132041d6a22c
karolis <karolis@usercentric.com.au>
2019-10-27 15:10:03 +1100

    wip

--- src/cms/preview-templates/IndexPagePreview.js
+++ src/cms/preview-templates/IndexPagePreview.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { IndexPageTemplate } from '../../templates/index-page'
+import React from "react"
+import PropTypes from "prop-types"
+import { IndexPageTemplate } from "../../templates/index-page"
 
 const IndexPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
+  const data = entry.getIn(["data"]).toJS()
 
   if (data) {
     return (
       <IndexPageTemplate
         image={data.image}
         title={data.title}
+        helloworld={data.helloworld}
         heading={data.heading}
         subheading={data.subheading}
         description={data.description}
@@ -24,9 +25,9 @@ const IndexPagePreview = ({ entry, getAsset }) => {
 
 IndexPagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  getAsset: PropTypes.func,
+  getAsset: PropTypes.func
 }
 
 export default IndexPagePreview
